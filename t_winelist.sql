bd47814ad6e01a9b9e8504ecaa5fc9df3068ad3a
Matthew Gallagher <mattva01@gmail.com>
2011-05-30 13:29:17 -0400

    Added sql to read in winelist

---
+++ t_winelist.sql
@@ -0,0 +1,9 @@
+BEGIN TRANSACTION;
+INSERT INTO "vinovoter_winevariety" VALUES(1,'Red','Zinfandel');
+INSERT INTO "vinovoter_winevariety" VALUES(2,'Red','Pinot Noir');
+INSERT INTO "vinovoter_winevariety" VALUES(3,'Red','Cabernet');
+INSERT INTO "vinovoter_winevariety" VALUES(4,'White','Chardonnay');
+INSERT INTO "vinovoter_winevariety" VALUES(5,'White','Pinot Gregio');
+INSERT INTO "vinovoter_winevariety" VALUES(6,'White','Sauvignon Blanc');
+COMMIT;
+
